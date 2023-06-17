11586fce8812c8c63992f3a918566ea5ba2945ab
TomHa <tomhackbarth@gmail.com>
2023-06-17 07:57:16 -0400

    Initial Commit

---
+++ LearnSwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearnSwiftData
+//
+//  Created by tom hackbarth on 6/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearnSwiftData/LearnSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftDataApp.swift
+//  LearnSwiftData
+//
+//  Created by tom hackbarth on 6/17/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
